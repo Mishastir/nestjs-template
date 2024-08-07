c6258586393a97b53218dc25a6fe5ff30662291f
Misha Masych <mishastir@gmail.com>
2024-08-07 13:36:02 +0300

    remove rate-limits, update packages, add custom script, fix husky, fix circular

--- src/common/session/dto/session-with-user.dto.ts
+++ src/common/session/dto/session-with-user.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-import { UserSessionDto } from "./index";
+import { UserSessionDto } from "./user-session.dto";
 
 import { UserEntity } from "@module/modules/users/entities";
 
